251e38c0e1c10fd50d0159b7ce8319fddd37127d
m-yasir-khan <yasir.khan@mach3bi.com>
2021-12-19 18:08:37 +0500

    ADD

---
+++ src/core/config.js
@@ -0,0 +1,10 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAN1cRIi0WfNmY-ynbb0dVzbYejvNhuUak",
+  authDomain: "expo-app-bec0b.firebaseapp.com",
+  projectId: "expo-app-bec0b",
+  storageBucket: "expo-app-bec0b.appspot.com",
+  messagingSenderId: "235351403324",
+  appId: "1:235351403324:web:9106d9137f0c7c9725cc39"
+}
+
